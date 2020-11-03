39ff7ebeab38ea9cf59bb92c9760044114c3b4b4
Tasha <xtasherx@gmail.com>
2020-11-03 00:21:53 -0600

    routes for venues working//added routes to randomly generate infor from db for both allBands and allVenues

--- public/js/venueProfile.js
+++ public/js/venueProfile.js
@@ -4,6 +4,8 @@ $.get("/api/user_data",(data) => {
 }).then((data) => {
   $.get("/api/venueData/" + data.id,(data) => {
     console.log(data);
+    const venueName = document.querySelector(".venueName");
+    venueName.innerHTML = data.venueName;
   });
 }).catch((err) => {
   res.status(401).json(err);
